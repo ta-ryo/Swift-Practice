5747fe4fa0633a721632cd44a28ff971f73cfdb5
ta-ryo <e165716@ie.u-ryukyu.ac.jp>
2020-01-07 13:10:27 +0900

    Initial Commit

---
+++ photo_shot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photo_shot
+//
+//  Created by 上原良太 on 2020/01/07.
+//  Copyright © 2020 上原良太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
